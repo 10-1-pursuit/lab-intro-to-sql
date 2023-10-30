78fc6f4abd19b7a7891583f608ab09016e2d927d
Jeremy Montes <jeremymontes@pursuit.org>
2023-10-30 16:40:55 -0400

    changed price to value

--- regifter.sql
+++ regifter.sql
@@ -16,7 +16,7 @@ CREATE DATABASE regifter;
 -- value - integer
 -- previously_regifted boolean
 
-CREATE TABLE gifts (id SERIAL PRIMARY KEY, gift TEXT,giver TEXT,value INT, previously_regifted BOOL);
+CREATE TABLE gifts (id SERIAL PRIMARY KEY, gift TEXT,giver TEXT,price INT, previously_regifted BOOL);
 
 
 -- 
@@ -28,7 +28,7 @@ CREATE TABLE gifts (id SERIAL PRIMARY KEY, gift TEXT,giver TEXT,value INT, previ
 \echo Alter the table so that the column price is changed to value 
 -- 
 
-ALTER TABLE gifts RENAME value TO value;
+ALTER TABLE gifts RENAME price TO value;
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
